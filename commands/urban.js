f14422d35df1ab40978acfef79a3446bc3f7763c
Kurt Yjares <kurt.m.m.yjares@accenture.com>
2019-11-07 14:17:26 +0800

    Added Urban Search Command

---
+++ commands/urban.js
@@ -0,0 +1,32 @@
+var urban = require("urban");
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "urban",
+  description: "Search for the internet's definition",
+  execute(message, args) {
+    let term = args.toString();
+    let cleanTerm = term.replace(/,/g, " ");
+    let search = urban(cleanTerm);
+
+    search.first(res => {
+      if (!res) message.channel.send("No results found");
+
+      let { word, definition, example, thumbs_up, thumbs_down, author } = res;
+
+      const urbanEmbed = new Discord.RichEmbed()
+        .setColor("0xffd465")
+        .setTitle(word)
+        .setDescription(definition)
+        .setAuthor(author)
+        .addField("Example", example, true)
+        .addField(
+          "Thumbs",
+          ":thumbsup: " + thumbs_up + " | :thumbsdown:" + thumbs_down,
+          true
+        );
+
+      message.channel.send(urbanEmbed);
+    });
+  }
+};
