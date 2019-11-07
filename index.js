3268e5d459c883617e844fc65c983059babaf663
Kurt Yjares <kurt.m.m.yjares@accenture.com>
2019-11-07 10:00:31 +0800

    Undeleted index.js(no idea how that happened

---
+++ index.js
@@ -0,0 +1,111 @@
+const fs = require("fs");
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+const jackie = new Discord.Client();
+jackie.commands = new Discord.Collection();
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  // set a new item in the Collection
+  // with the key as the command name and the value as the exported module
+  jackie.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
+changeStatus = async () => {
+  while (true) {
+    await jackie.user.setPresence({
+      status: "online",
+      game: { name: "nice with her bot friends" }
+    });
+    await new Promise(resolve => setTimeout(resolve, 10000));
+    await jackie.user.setPresence({
+      status: "dnd",
+      game: { name: "house" }
+    });
+    await new Promise(resolve => setTimeout(resolve, 10000));
+    await jackie.user.setPresence({
+      status: "idle",
+      game: { name: "Playing Playing" }
+    });
+    await new Promise(resolve => setTimeout(resolve, 10000));
+  }
+};
+
+jackie.on("ready", async () => {
+  console.log(
+    `Good day father\nMy name is ${jackie.user.tag}\nMy ID is ${jackie.user.id}`
+  );
+
+  changeStatus();
+});
+
+jackie.on("message", message => {
+  // console.log(message.content);
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const commandName = args.shift().toLowerCase();
+  const command =
+    jackie.commands.get(commandName) ||
+    jackie.commands.find(
+      cmd => cmd.aliases && cmd.aliases.includes(commandName)
+    );
+
+  if (!command) return;
+
+  //check if command is server only
+  if (command.guildOnly && message.channel.type !== "text") {
+    return message.reply("I can't execute that command inside DMs!");
+  }
+
+  //check arguments
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+    }
+
+    return message.channel.send(reply);
+  }
+
+  //cooldown
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(
+        `please wait ${timeLeft.toFixed(
+          1
+        )} more second(s) before reusing the \`${command.name}\` command.`
+      );
+    }
+  }
+
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
+  }
+});
+
+jackie.login(token);
