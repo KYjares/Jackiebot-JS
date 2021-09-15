79e56d6420e1b57a75bb0ca7a9eb8678aff34c41
Cascade <kazkadezed@gmail.com>
2021-09-15 12:19:34 +0800

    Fixed help command

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js");
 require("dotenv").config();
 
 module.exports = {
@@ -7,18 +8,18 @@ module.exports = {
 	usage: "[command name]",
 	cooldown: 5,
 	execute(message, args) {
-		const data = [];
 		const { commands } = message.client;
+		var helpEmbed;
 
 		if (!args.length) {
-			data.push("Here's a list of all my commands:");
-			data.push(commands.map((command) => command.name).join(", "));
-			data.push(
-				`\nYou can send \`${process.env.PREFIX}help [command name]\` to get info on a specific command!`
+			helpEmbed = new Discord.MessageEmbed().addField(
+				"Here's a list of all my commands:",
+				commands.map((command) => command.name).join(", ") +
+					`\n\nYou can send \`${process.env.PREFIX}help [command name]\` to get info on a specific command!`
 			);
 
-			return message.author
-				.send(data, { split: true })
+			message.author
+				.send({ embeds: [helpEmbed] })
 				.then(() => {
 					if (message.channel.type === "DM") return;
 					message.reply("I've sent you a DM with all my commands!");
@@ -32,30 +33,33 @@ module.exports = {
 						"it seems like I can't DM you! Do you have DMs disabled?"
 					);
 				});
-		}
+		} else {
+			const name = args[0].toLowerCase();
+			const command =
+				commands.get(name) ||
+				commands.find((c) => c.aliases && c.aliases.includes(name));
 
-		const name = args[0].toLowerCase();
-		const command =
-			commands.get(name) ||
-			commands.find((c) => c.aliases && c.aliases.includes(name));
+			if (!command) {
+				return message.reply("that's not a valid command!");
+			}
 
-		if (!command) {
-			return message.reply("that's not a valid command!");
-		}
-
-		data.push(`**Name:** ${command.name}`);
-
-		if (command.aliases)
-			data.push(`**Aliases:** ${command.aliases.join(", ")}`);
-		if (command.description)
-			data.push(`**Description:** ${command.description}`);
-		if (command.usage)
-			data.push(
-				`**Usage:** ${process.env.PREFIX}${command.name} ${command.usage}`
+			helpEmbed = new Discord.MessageEmbed().setTitle(
+				`**Name:** ${command.name}`
 			);
 
-		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+			if (command.aliases)
+				helpEmbed.addField("**Aliases:**", command.aliases.join(", "));
+			if (command.description)
+				helpEmbed.addField("**Description:**", command.description);
+			if (command.usage)
+				helpEmbed.addField(
+					"**Usage:**",
+					process.env.PREFIX + command.name + command.usage
+				);
 
-		message.channel.send(data, { split: true });
+			helpEmbed.addField("**Cooldown:**", command.cooldown || 3 + " second(s)");
+
+			message.channel.send({ embeds: [helpEmbed] });
+		}
 	},
 };
